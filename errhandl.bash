64066d0172a8c42c0db819ff56d53c0ba1caa518
Andrew Rogers <targetdisk1394@gmail.com>
2023-05-04 14:38:30 -0500

    Added unhappy_ending to happy wrappy
    Now happy prints the command line that failed before exiting!

--- errhandl.bash
+++ errhandl.bash
@@ -1,2 +1,7 @@
 # Don't want subsequent commands to run after nonzero exit?  Wrappy in happy!
-happy() { $@ || exit $?; }
+unhappy_ending() {
+  stat=$1; shift
+  (echo "Error while running: \"$@\"" >&2)
+  exit $stat
+}
+happy() { $@ || unhappy_ending $? $@; }
