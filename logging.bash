68195a72be0d0c991e6c9fb3ec45d4f25327784b
Andrew Rogers <targetdisk1394@gmail.com>
2023-10-09 02:32:58 -0500

    logging: $usage helper and more cat-like helpers

--- logging.bash
+++ logging.bash
@@ -1,5 +1,8 @@
+define_usage() { read -d '' usage; }
 errcho() { (>&2 echo $@); }
+ercat() { (>&2 cat "$@"); }
 errcat() { (>&2 cat <<<"$@"); }
 dedcat() { (>&2 cat <<<"$@"); exit 1; }
+xcat() { (>&2 cat "$@"); exit 0; }
 excat() { (>&2 cat <<<"$@"); exit 0; }
 die() { errcho $@; exit 1; }
